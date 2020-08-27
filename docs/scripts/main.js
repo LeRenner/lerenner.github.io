fa55d3245b731c0ed6b6e4202ee11f1824c6e39b
J. Renner <j@renner.pw>
2020-08-27 16:34:06 -0300

    Change icon sizes for mobile website
    Signed-off-by: J. Renner <j@renner.pw>

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -4,19 +4,21 @@ var contact = document.getElementById("contact");
 var whoami = document.getElementById("whoami");
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
-var copyMail = document.getElementById("copyMail")
-var copyTelegram = document.getElementById("copyTelegram")
-var copyDiscord = document.getElementById("copyDiscord")
+var copyMail = document.getElementById("copyMail");
+var copyTelegram = document.getElementById("copyTelegram");
+var copyDiscord = document.getElementById("copyDiscord");
 var db1 = document.getElementById("deskBreaker1");
 var db2 = document.getElementById("deskBreaker2");
-var logo = document.getElementById("logo")
-console.log("batatas")
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+console.log("batatas");
 
 copyMail.onclick = function() {
   var copy = navigator.clipboard.writeText("j@lerenner.dev")
   copyMail.innerHTML = "Copied!";
   setTimeout(function(){
-    copyMail.innerHTML = "Copy"
+    copyMail.innerHTML = "Copy";
   }, 320);
 }
 
@@ -64,6 +66,9 @@ window.onclick = function(event) {
 if (screen.width <= 699) {
   db1.style.display = "none";
   db2.style.display = "none";
-  logo.style.fontSize = "60%";
+  logo.style.fontSize = "8px";
+  img1.style.height = "42";
+  img2.style.height = "42";
+  img3.style.height = "42";
 }
 
