41caee498162a97a3caea22d4eeb94bc348fb1ee
J. Renner <j@lerenner.dev>
2020-11-04 11:26:10 -0300

    Temporarily remove copy on mobile
    Signed-off-by: J. Renner <j@lerenner.dev>

--- docs/main.js
+++ docs/main.js
@@ -2,20 +2,40 @@ var copyMail = document.getElementById("copyMail");
 var copyTelegram = document.getElementById("copyTelegram");
 var copyDiscord = document.getElementById("copyDiscord");
 
-copyMail.addEventListener("click touchstart", function(){
-	var copy = navigator.clipboard.writeText("j@lerenner.dev");
+copyMail.onclick = function() {
+  var copy = navigator.clipboard.writeText("j@lerenner.dev");
   copyMail.innerHTML = "Copied!";
   setTimeout(function(){copyMail.innerHTML = "j@lerenner.dev";}, 320);
-});
+}
 
-copyTelegram.addEventListener("click touchstart", function(){
+copyTelegram.onclick = function() {
   var copy = navigator.clipboard.writeText("LeRenner");
   copyTelegram.innerHTML = "Copied!";
   setTimeout(function(){copyTelegram.innerHTML = "@lerenner"}, 320);
-});
+}
 
-copyDiscord.addEventListener("click touchstart", function(){
+copyDiscord.onclick = function() {
   var copy = navigator.clipboard.writeText("LojasRenner#3419");
   copyDiscord.innerHTML = "Copied!";
   setTimeout(function(){copyDiscord.innerHTML = "LojasRenner#3419"}, 320);
-});
+}
+
+//for mobile only:
+
+mobileCopyMail = function() {
+	var copy = navigator.clipboard.writeText("j@lerenner.dev");
+	copyMail.innerHTML = "Copied!";
+	setTimeout(function(){copyMail.innerHTML = "j@lerenner.dev";}, 320);
+}
+
+mobileCopyTelegram = function() {
+	var copy = navigator.clipboard.writeText("LeRenner");
+  copyTelegram.innerHTML = "Copied!";
+  setTimeout(function(){copyTelegram.innerHTML = "@lerenner"}, 320);
+}
+
+mobileCopyDiscord = function() {
+	var copy = navigator.clipboard.writeText("LojasRenner#3419");
+  copyDiscord.innerHTML = "Copied!";
+  setTimeout(function(){copyDiscord.innerHTML = "LojasRenner#3419"}, 320);
+}
