85fb575624d39a5317b31420828c621c6a5ce5be
J. Renner <j@lerenner.dev>
2020-11-04 10:59:34 -0300

    Improve mobile rendering
    Signed-off-by: J. Renner <j@lerenner.dev>

--- docs/main.js
+++ docs/main.js
@@ -2,20 +2,20 @@ var copyMail = document.getElementById("copyMail");
 var copyTelegram = document.getElementById("copyTelegram");
 var copyDiscord = document.getElementById("copyDiscord");
 
-copyMail.onclick = function() {
-  var copy = navigator.clipboard.writeText("j@lerenner.dev");
+copyMail.addEventListener("click", function(){
+	var copy = navigator.clipboard.writeText("j@lerenner.dev");
   copyMail.innerHTML = "Copied!";
   setTimeout(function(){copyMail.innerHTML = "j@lerenner.dev";}, 320);
-}
+});
 
-copyTelegram.onclick = function() {
+copyTelegram.addEventListener("click", function(){
   var copy = navigator.clipboard.writeText("LeRenner");
   copyTelegram.innerHTML = "Copied!";
   setTimeout(function(){copyTelegram.innerHTML = "@lerenner"}, 320);
-}
+});
 
-copyDiscord.onclick = function() {
+copyDiscord.addEventListener("click", function(){
   var copy = navigator.clipboard.writeText("LojasRenner#3419");
   copyDiscord.innerHTML = "Copied!";
   setTimeout(function(){copyDiscord.innerHTML = "LojasRenner#3419"}, 320);
-}
+});
