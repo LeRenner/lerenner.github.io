415039c4691be73a0ba58787863b62b11eb0de8d
J. Renner <j@lerenner.dev>
2020-11-18 17:03:27 -0300

    Fix underline decoration on text copy
    Signed-off-by: J. Renner <j@lerenner.dev>

--- docs/main.js
+++ docs/main.js
@@ -7,28 +7,40 @@ copyMailDev.onclick = function() {
   var copy = navigator.clipboard.writeText("j@lerenner.dev");
 	copyMailDev.style.textDecoration = "none";
   copyMailDev.innerHTML = "<pre style='margin:0;color: #aaa;'>         Copied!</pre>";
-  setTimeout(function(){copyMailDev.innerHTML = "j@lerenner.dev";}, 320);
+  setTimeout(function(){
+		copyMailDev.innerHTML = "j@lerenner.dev";
+		copyMailDev.style.textDecoration = "underline";
+	}, 320);
 }
 
 copyMailPw.onclick = function() {
   var copy = navigator.clipboard.writeText("j@renner.pw");
 	copyMailPw.style.textDecoration = "none";
   copyMailPw.innerHTML = "<pre style='margin:0;color: #aaa;'>         Copied!</pre>";
-  setTimeout(function(){copyMailPw.innerHTML = "j@renner.pw";}, 320);
+  setTimeout(function(){
+		copyMailPw.innerHTML = "j@renner.pw";
+		copyMailPw.style.textDecoration = "underline";
+	}, 320);
 }
 
 copyTelegram.onclick = function() {
   var copy = navigator.clipboard.writeText("LeRenner");
 	copyTelegram.style.textDecoration = "none";
   copyTelegram.innerHTML = "<pre style='margin:0;color: #aaa;'>         Copied!</pre>";
-  setTimeout(function(){copyTelegram.innerHTML = "@lerenner"}, 320);
+  setTimeout(function(){
+		copyTelegram.innerHTML = "@lerenner";
+		copyTelegram.style.textDecoration = "underline";
+	}, 320);
 }
 
 copyDiscord.onclick = function() {
   var copy = navigator.clipboard.writeText("LojasRenner#3419");
 	copyDiscord.style.textDecoration = "none";
   copyDiscord.innerHTML = "<pre style='margin:0;color: #aaa;'>         Copied!</pre>";
-  setTimeout(function(){copyDiscord.innerHTML = "LojasRenner#3419"}, 320);
+  setTimeout(function(){
+		copyDiscord.innerHTML = "LojasRenner#3419";
+		copyDiscord.style.textDecoration = "underline";
+	}, 320);
 }
 
 //animation
